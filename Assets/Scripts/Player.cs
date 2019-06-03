6ffb58809930f150b35c12ed73c5256a1380754b
Ogawatugawa <vin.tansiri@gmail.com>
2019-06-03 16:09:21 +1000

    Climbing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,18 @@ using Prime31;
 [RequireComponent(typeof(CharacterController2D))]
 public class Player : MonoBehaviour
 {
-    public float movementSpeed = 10f;
+    public float moveSpeed = 10f;
     public float jumpHeight = 10f;
     public float gravity = -10f;
+    public float centreRadius = 0.5f;
     public bool IsRight = true;
     public bool IsJumping = false;
+    public bool IsClimbing = false;
     public SpriteRenderer rend;
     public CharacterController2D controller;
     public Animator anim;
 
-    public Vector3 motion; // Store the difference in movement
+    public Vector3 velocity; // Store the difference in movement
     private void Reset()
     {
         controller = GetComponent<CharacterController2D>();
@@ -23,20 +25,26 @@ public class Player : MonoBehaviour
         rend = GetComponent<SpriteRenderer>();
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, centreRadius);
+    }
+
     void Update()
     {
         // Get horizontal and vertical input
         float inputH = Input.GetAxis("Horizontal");
-        float inputV = Input.GetAxis("Vertical");
+        float inputV = Input.GetAxisRaw("Vertical");
 
         // Apply gravity
-        motion.y += gravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
         // If player is standing on ground
         if (controller.isGrounded)
         {
             // Set downward movement to 0 
-            motion.y = 0f;
-            anim.SetBool("IsJumping", false);
+            velocity.y = 0f;
+            anim.SetBool("IsGrounded", true);
 
             // If we press Jump
             if (Input.GetButtonDown("Jump"))
@@ -46,20 +54,24 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (anim.GetBool("IsJumping"))
+        if (!anim.GetBool("IsGrounded"))
         {
-            anim.SetFloat("JumpY", motion.y);
+            anim.SetFloat("JumpY", velocity.y);
         }
 
         // Climb up or down depending on Y value
-        Climb(inputV);
-        // Move left or right depending on X value
-        Move(inputH);
+        Climb(inputH, inputV);
+
+        if (!IsClimbing)
+        {
+            // Move left or right depending on X value
+            Move(inputH);
+        }
 
         Crouch(inputV);
 
         // Move using Character Controller 2D
-        controller.Move(motion * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
 
         //Animation();
     }
@@ -67,14 +79,67 @@ public class Player : MonoBehaviour
     public void Move(float inputH)
     {
         // Generate movement for left/right
-        motion.x = inputH * movementSpeed;
+        velocity.x = inputH * moveSpeed;
         anim.SetBool("IsRunning", inputH != 0);
-        rend.flipX = inputH < 0;
+        if (inputH != 0)
+        {
+            rend.flipX = inputH <= 0;
+        }
     }
 
-    public void Climb(float inputV)
+    public void Climb(float inputH, float inputV)
     {
+        bool IsOverLadder = false;
 
+        #region Part 1 - Detecting Ladders
+        // Get a list of all hit objects overlapping point
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, centreRadius);
+
+        // Loop through all hit objects
+        foreach (var hit in hits)
+        {
+            if (hit.tag == "Ground")
+            {
+                IsClimbing = false;
+                IsOverLadder = false;
+                break;
+            }
+            // Check if tagged "Ladder"
+            if (hit.tag == "Ladder")
+            {
+                // Player is overlapping a Ladder!
+                IsOverLadder = true;
+                break; // Exit just the foreach loop if we are over a ladder
+            }
+        }
+        // If the player is overlapping AND input vertical is made
+        if (IsOverLadder && inputV != 0)
+        {
+            //    The player is in Climbing state!
+            IsClimbing = true;
+        }
+        #endregion
+
+        #region Part 2 - Translating the Player
+
+        Vector3 inputDir = new Vector3(inputH, inputV, 0);
+        // If player is climbing
+        if (IsClimbing)
+        {
+            anim.SetBool("IsClimbing", true);
+            velocity.y = 0;
+            transform.Translate(inputDir * moveSpeed * Time.deltaTime);
+        }
+        //    Move player up and down on the ladder (additionally move left and right)
+        #endregion
+        if (!IsOverLadder)
+        {
+            anim.SetBool("IsClimbing", false);
+            anim.SetBool("IsClimbing", IsClimbing);
+            IsClimbing = false;
+        }
+
+        anim.SetFloat("ClimbSpeed", inputDir.magnitude * moveSpeed);
     }
 
     public void Hurt()
@@ -84,8 +149,7 @@ public class Player : MonoBehaviour
 
     public void Jump()
     {
-        motion.y += jumpHeight;
-        anim.SetBool("IsJumping", true);
+        velocity.y = jumpHeight;
     }
 
     public void Crouch(float inputV)
